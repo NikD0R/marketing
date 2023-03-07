deb16e2fa4355720f7aaefcadad03acd80ef197d
Nikita Doroshenko <nikitokd05@gmail.com>
2023-03-08 00:43:01 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,35 @@
 $(function(){
+
+  $(".logo, .menu a, .footer-top__go-top, .footer-top__logo").on("click", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
   $('.slider-blog__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="left arrow"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="right arrow"></button>',
+    responsive: [
+    {
+      breakpoint: 760,
+      settings: {
+        arrows: false,
+      }
+    }
+  ]
+  });
+
+  $('.menu__btn, .menu a').on('click', function() {
+    $('.header__top-inner').toggleClass('header__top-inner--active');
   });
 
 
